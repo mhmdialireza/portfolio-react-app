4f11cdc29914e226d007fef7222d19ad8582829b
Alireza Mohammadi <mhmdi.alireza8035@gmail.com>
2023-07-15 13:43:39 +0330

    Add Letter News section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,45 @@
-import { FooterItem } from "./../../components";
+import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import { FooterItem, Input } from "./../../components";
+import swal from "sweetalert";
+import { emailValidator } from "../../validators/rules";
 
 import "./footer.css";
 
 const Footer = () => {
+  const [formState, onInputHandler] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const addNewEmail = (event) => {
+    event.preventDefault();
+    const newEmail = {
+      email: formState.inputs.email.value,
+    };
+
+    fetch("http://localhost:4000/v1/newsletters", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newEmail),
+    }).then((res) => {
+      if (res.ok) {
+        swal({
+          title: "ایمیل شما با موفقیت در خبرنامه ثبت شد",
+          icon: "success",
+          buttons: "خیلی هم عالی",
+        });
+      }
+    });
+  };
+
   return (
     <footer className="footer">
       <div className="container">
@@ -83,6 +120,38 @@ const Footer = () => {
                   <a href="#" className="footer-widgets__link">
                     آموزش پایتون
                   </a>
+                </div>
+
+                <div className="col-6">
+                  <Link to="/contact" className="footer-widgets__link">
+                    ارتباط با ما
+                  </Link>
+                </div>
+                <div className="col-12">
+                  <span className="footer-widgets__title">
+                    اشتراک در خبرنامه
+                  </span>
+                  <span className="footer-widgets__text text-center d-block">
+                    جهت اطلاع از آخرین اخبار و تخفیف های سایت مشترک شوید!
+                  </span>
+                  <form action="#" className="footer-widgets__form">
+                    <Input
+                      element="input"
+                      id="email"
+                      type="text"
+                      className="footer-widgets__input"
+                      placeholder="ایمیل خود را وارد کنید."
+                      onInputHandler={onInputHandler}
+                      validations={[emailValidator()]}
+                    />
+                    <button
+                      type="submit"
+                      className="footer-widgets__btn"
+                      onClick={addNewEmail}
+                    >
+                      عضویت
+                    </button>
+                  </form>
                 </div>
               </div>
             </FooterItem>
